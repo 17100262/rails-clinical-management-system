33077ad3cd0c22273f9a0fe8ee2a65b5997b27e0
a17100262 <maueezahmed@gmail.com>
2018-04-13 11:46:52 +0000

    translation tweak

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Freelance
     config.active_job.queue_adapter = :sidekiq
     I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :pt
+    I18n.locale = config.i18n.locale = config.i18n.default_locale
     # config.i18n.available_locales = [:en, :pt]
 
 
